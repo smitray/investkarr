f0a80a3d6fb7181a531e1f11c3b5672e1f5017c4
smitray <humancoder@outlook.com>
2022-01-23 00:49:27 +0530

    :recycle: refactor(authlayout): updated and refactored
    with button to be inside

--- src/modules/auth/signup/with-email.tsx
+++ src/modules/auth/signup/with-email.tsx
@@ -1,8 +1,7 @@
-import { Box, Layout, Text, Button, TextInput } from '@ui';
+import { TextInput } from '@ui';
 import React, { useCallback } from 'react';
 import { Formik } from 'formik';
 import * as Yup from 'yup';
-import { TouchableWithoutFeedback, Keyboard } from 'react-native';
 import { AuthLayout } from '@components';
 
 type FormValues = {
@@ -25,42 +24,35 @@ const WithEmail = () => {
   }, []);
 
   return (
-    <AuthLayout
-      title="Nice to meet you"
-      description="Choose the email you would like to use to \n log in to your account"
-    >
-      <TouchableWithoutFeedback onPress={() => Keyboard.dismiss()}>
-        <Box flex={4} alignItems={'center'}>
-          <Formik
-            {...{ initialValues, onSubmit, validationSchema }}
-            validateOnChange
-          >
-            {({
-              handleSubmit,
-              values,
-              handleChange,
-              handleBlur,
-              touched,
-              errors,
-            }) => (
-              <>
-                <TextInput
-                  label="Enter email"
-                  value={values.email}
-                  onChangeText={handleChange('email')}
-                  onBlur={handleBlur('email')}
-                  keyboardType="email-address"
-                  autoCompleteType="username"
-                  textContentType="emailAddress"
-                  errorMessage={errors.email && touched.email && errors.email}
-                />
-                <Button label="Send OTP" onPress={handleSubmit} />
-              </>
-            )}
-          </Formik>
-        </Box>
-      </TouchableWithoutFeedback>
-    </AuthLayout>
+    <Formik {...{ initialValues, onSubmit, validationSchema }} validateOnChange>
+      {({
+        handleSubmit,
+        values,
+        handleChange,
+        handleBlur,
+        touched,
+        errors,
+      }) => (
+        <AuthLayout
+          title="Nice to meet you"
+          description="Choose the email you would like to use to \n log in to your account"
+          label="Send OTP"
+          onPress={handleSubmit}
+          disabled={errors.email ? true : false}
+        >
+          <TextInput
+            label="Enter email"
+            value={values.email}
+            onChangeText={handleChange('email')}
+            onBlur={handleBlur('email')}
+            keyboardType="email-address"
+            autoCompleteType="username"
+            textContentType="emailAddress"
+            errorMessage={errors.email && touched.email && errors.email}
+          />
+        </AuthLayout>
+      )}
+    </Formik>
   );
 };
 
--- src/shared/components/auth-layout.tsx
+++ src/shared/components/auth-layout.tsx
@@ -1,15 +1,30 @@
 import React from 'react';
-import { Box, Layout, Text } from '@ui';
-import { KeyboardAvoidingView, Platform } from 'react-native';
+import { Box, Button, Layout, Text } from '@ui';
+import {
+  KeyboardAvoidingView,
+  Platform,
+  TouchableWithoutFeedback,
+  Keyboard,
+} from 'react-native';
 import { Children } from '@tp/global';
 
 type AuthLayoutProperties = {
   title: string;
   description: string;
   children: Children;
+  label: string;
+  onPress: () => void;
+  disabled: boolean;
 };
 
-const AuthLayout = ({ title, description, children }: AuthLayoutProperties) => {
+const AuthLayout = ({
+  title,
+  description,
+  children,
+  label,
+  onPress,
+  disabled = false,
+}: AuthLayoutProperties) => {
   return (
     <KeyboardAvoidingView
       behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
@@ -30,7 +45,14 @@ const AuthLayout = ({ title, description, children }: AuthLayoutProperties) => {
             {description.replace(/\\n/g, '\n')}
           </Text>
         </Box>
-        {children}
+        <TouchableWithoutFeedback onPress={() => Keyboard.dismiss()}>
+          <Box flex={4} alignItems={'center'}>
+            {children}
+          </Box>
+        </TouchableWithoutFeedback>
+        <Box flex={1}>
+          <Button label={label} onPress={onPress} disabled={disabled} />
+        </Box>
       </Layout>
     </KeyboardAvoidingView>
   );
